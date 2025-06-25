68a51c933126a2f7f56704e0141215ba38a3273e
Piotr Betlej <104993519+mo1der@users.noreply.github.com>
2025-06-25 13:03:46 +0200

    Update ddl_bronze.sql

--- sql/project-online-retail/scripts/bronze/ddl_bronze.sql
+++ sql/project-online-retail/scripts/bronze/ddl_bronze.sql
@@ -9,82 +9,20 @@ Script Purpose:
 ===============================================================================
 */
 
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+USE [OnlineRetail];
+
+IF OBJECT_ID('bronze.online_retail', 'U') IS NOT NULL
+    DROP TABLE bronze.online_retail;
 GO
 
-CREATE TABLE bronze.crm_cust_info (
-    cst_id              INT,
-    cst_key             NVARCHAR(50),
-    cst_firstname       NVARCHAR(50),
-    cst_lastname        NVARCHAR(50),
-    cst_marital_status  NVARCHAR(50),
-    cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE
-);
-GO
-
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info;
-GO
-
-CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
-);
-GO
-
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details;
-GO
-
-CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
-);
-GO
-
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
-GO
-
-CREATE TABLE bronze.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
-);
-GO
-
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12;
-GO
-
-CREATE TABLE bronze.erp_cust_az12 (
-    cid    NVARCHAR(50),
-    bdate  DATE,
-    gen    NVARCHAR(50)
-);
-GO
-
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
-GO
-
-CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+CREATE TABLE bronze.online_retail (
+	invoice_no		VARCHAR(10),	-- Handles 6-digit numbers & optional 'C' prefix
+	stock_code		VARCHAR(10),	-- Also nominal, even if mostly numeric
+	description		TEXT,			-- Free-text product description
+	quantity		INT,			-- Integer quantity (can be negative for returns)
+	invoice_date	DATETIME,		-- Stores date and time together
+	unit_price		DECIMAL(10, 2),	-- Up to 99999999.99 in GBP (£)
+	customer_id		INT,			-- 5-digit integer
+	country			NVARCHAR(50)	-- Country name
 );
 GO
